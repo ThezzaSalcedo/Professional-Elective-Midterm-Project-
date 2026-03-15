d76dc527fd927cd6a11a323256db21b0fbd5b229
Thezzalia Mae B. Salcedo <thezzaliamae.salcedo@neu.edu.ph>
2026-03-15 23:49:26 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,6 +15,7 @@ import {
 } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
 export default function DashboardPage() {
