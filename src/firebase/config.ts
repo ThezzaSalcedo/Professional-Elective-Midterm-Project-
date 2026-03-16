7465d70254702cefd47b52d6a1837fb2e89c58ad
Thezzalia Mae B. Salcedo <thezzaliamae.salcedo@neu.edu.ph>
2026-03-16 02:42:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3971650334-60abc",
+  "appId": "1:150362613017:web:a5771abcff5607aaf25188",
+  "apiKey": "AIzaSyCKafJPFqozW-e1b0eQsC9SpAYt1mxqDRI",
+  "authDomain": "studio-3971650334-60abc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "150362613017"
+};
